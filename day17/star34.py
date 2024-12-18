1798be045410de2d0c690f4447634d78a82a8dba
jlazear <jlazear@gmail.com>
2024-12-18 03:26:42 -0800

    3 bits too clever

--- day17/star34.py
+++ day17/star34.py
@@ -109,7 +109,6 @@ def op(
 i = 0
 output = op(digits)
 while output != instructions:
-    # print(f"\n{i = } {digits = }\n{output = }\ninstru = {instructions}")  # DELME
     if digits[i] >= 7:
         digits[i] = 0
         digits[i - 1] += 1
@@ -121,99 +120,16 @@ while output != instructions:
         digits[i] += 1
     output = op(digits)
     if output[-i - 1] == instructions[-i - 1]:
-        # print(f"last digit match {output[-i - 1] = },  {instructions[-i - 1] = }")
         i += 1
 
 
-print(digits)
 a = a_from_digits(digits)
-# print(f"{a = }")
-# print(run((a, B, C), instructions))
-# print(instructions)
 
+# somehow the algorithm overshot?
 best_a = a
 for delta in range(8**6):
     new_a = a - delta
     new_output = run((new_a, B, C), instructions)
     if tuple(new_output) == instructions:
-        # print(f"{tuple(new_output) == instructions}")
-        # print(f"{new_output}")
-        # print(f"{instructions}")
-        # print(new_a)
         best_a = min(best_a, new_a)
 print(best_a)
-
-
-# for a in range(64):
-#     print(a, run((a, B, C), instructions))
-
-
-def find_bounds(B, C, instructions, offset: int = 0):
-    op = lambda a: len(run((a, B, C), instructions))
-    left: int = 1
-    right: int = 1
-    output_len: int = 0
-    while output_len < len(instructions) + offset:
-        right *= 2
-        print(f"{right = }")  # DELME
-        output_len = op(right)
-
-    while right - left >= 2:
-        print(f"{left = } {right = }")  # DELME
-        mid = (left + right) // 2
-        output_len = op(mid)
-        if output_len < len(instructions) + offset:
-            left = mid
-        else:
-            right = mid
-    return left, right
-
-
-# _, bottom = find_bounds(B, C, instructions)
-# top, _ = find_bounds(B, C, instructions, 1)
-#
-# print(run((bottom, B, C), instructions))
-# print(run((top, B, C), instructions))
-# print(len(instructions))
-# print(top, bottom)
-# print(top - bottom)
-# print(math.log10(bottom), math.log2(bottom), math.log2(bottom) / math.log2(8))
-# print(math.log10(top), math.log2(top), math.log2(top) / math.log2(8))
-
-
-# for delta in range(0, (top - bottom), (top - bottom) // 256):
-#     a = bottom + delta
-#     print(f"{a = }")
-#     print(run((a, B, C), instructions))
-# # for i in range(128):
-# #     firsts = []
-# #     for delta in range(0, 9):
-# #         a = bottom + delta + 8 * i
-# #         firsts.append(run((a, B, C), instructions)[0])
-# #     print(firsts)
-
-# for a in range(bottom, top):
-#     if (a - bottom) % ((top - bottom) // 1_000_000) == 0:
-#         print(f"{a = } ({(a - bottom) / (top - bottom)})")
-#     registers = (a, B, C)
-#     output_list = run(registers, instructions)
-#     if tuple(output_list) == instructions:
-#         print(f"{a = }")
-#         break
-# a = 1
-# max_iter = 1000000
-# for i in range(max_iter):  # infinite loops bad
-#     registers = (a, B, C)
-#     output_list = run(registers, instructions, 0)
-#     if (i % (max_iter // 10)) == 0:
-#         print(f"{i = } / {max_iter}")
-#         print(f"{len(output_list) = }\n{output_list}")
-#     if tuple(output_list) == instructions:
-#         print(i)
-#         break
-#     if len(output_list) < len(instructions):
-#         a *= 10
-#         print(f"10x! new {a = }")
-#     a += 1
-# else:
-#     raise Exception("Infinite loop!")
